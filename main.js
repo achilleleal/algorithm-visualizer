b97c92aba5e9dcd6df1acba849dded74a81c61d4
achilleleal <achilleleal@gmail.com>
2021-03-20 19:55:25 -0400

    added selection sort

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 import './style.css'
 
-const view = document.getElementById("visualizer");
+import selectionSort from './src/selectionSort'
 
-document.getElementById("arrGenBtn").addEventListener('click', genRandArr)
+const view = document.getElementById("visualizer");
+let currentArray, viewItems;
+
+document.getElementById("arrGenBtn").addEventListener('click', () => currentArray = genRandArr())
+
+document.getElementById("selectionSortBtn").addEventListener('click', () => selectionSort(currentArray, viewItems))
 
 function genRandArr() {
 
   view.innerHTML = '';
 
   const array = [];
-  const arrLen = 500;
+  const arrLen = 750;
 
   for (let i = 0; i < arrLen; i++) {
     array.push(Math.random() * 500);
@@ -19,10 +24,10 @@ function genRandArr() {
     const bar = document.createElement('div');
     bar.classList.add('bar');
     bar.style.height = `${i}px`;
-    console.log(parseInt(bar.style.height))
 
     view.appendChild(bar)
   })
 
+  viewItems = view.children
   return array
 }
